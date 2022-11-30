87d2b01822bdfdbdde2f12450582d384cefeeb26
naetosuuke <118874380+naetosuuke@users.noreply.github.com>
2022-11-30 22:47:43 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by Daisuke Doi on 2022/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
